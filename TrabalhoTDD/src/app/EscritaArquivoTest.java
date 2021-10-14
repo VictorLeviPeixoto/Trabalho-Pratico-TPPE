69e5b221eecd93b07d8917ef5d9089ad7e570093
Victor Peixoto <victorlevipeixoto@gmail.com>
2021-10-14 20:45:08 -0300

    Adição estrutura base TDD

---
+++ TrabalhoTDD/src/app/EscritaArquivoTest.java
@@ -0,0 +1,10 @@
+package app;
+
+import org.junit.Test;
+
+public class EscritaArquivoTest {
+	@Test
+	private void testDefinirDelimitador() {
+		
+	}
+}
---
+++ TrabalhoTDD/src/test/EscritaArquivo.java
@@ -0,0 +1,7 @@
+package test;
+
+public class EscritaArquivo {
+	public void definirDelimitador() {
+		
+	}
+}
