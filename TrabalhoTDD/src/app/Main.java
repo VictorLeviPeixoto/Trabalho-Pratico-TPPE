f6af1aa5fb9b32f5f083a57202a13c3e9eb6d7b2
mikhaelle <mikhabueno@outlook.com>
2021-10-19 20:01:38 -0300

    [Refactor] fix main

--- TrabalhoTDD/src/app/Main.java
+++ TrabalhoTDD/src/app/Main.java
@@ -1,9 +1,13 @@
 package app;
 
+
+import java.io.IOException;
+
 import java.util.Scanner;
 
 import exceptions.ArquivoNaoEncontradoException;
 import exceptions.EscritaNaoPermitidaException;
+
 
 public class Main {
 
@@ -11,29 +15,36 @@ public class Main {
 		// TODO Auto-generated constructor stub
 	}
 
+
 	public static void main(String[] args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
 		LerArquivoAnalysisTime analysisTime = new LerArquivoAnalysisTime();
 		LerArquivoAnalysisMemory analysisMemory = new LerArquivoAnalysisMemory();
 		EscritaArquivo escritaArquivo = new EscritaArquivo();
+		NumeroAnalises numeroAnalysisTime = new NumeroAnalises();
+		NumeroAnalises numeroAanalysisMemory = new NumeroAnalises();
 
-		analysisTime.lerAnalysisTime();
-		analysisMemory.lerAnalysisMemory();
-		
+
+
+		System.out.print("Digite o delimitador que deseja usar no arquivo de saída: ");
+		String delimitador = scanner.nextLine();
+		escritaArquivo.definirDelimitador(delimitador);
 
 		System.out.print("Digite o caminho do diretório para salvar o arquivo de saída: ");
 		String caminhoArquivoSaida = scanner.nextLine();
 		escritaArquivo.setCaminhoSaida(caminhoArquivoSaida);
 		
 
-		System.out.print("Escolha o formato de saída: linhas ou colunas");
+		System.out.print("Escolha o formato de saída linhas ou colunas: ");
 		String formatoArquivoSaida = scanner.nextLine();
 		escritaArquivo.setFormato(formatoArquivoSaida);
 		
-		scanner.close();
 		
-		
+		numeroAnalysisTime.getNumeroAnalysisTime();
+		numeroAanalysisMemory.getNumeroAnalysisMemory();
 
+
+		scanner.close();
 	}
 
 }
